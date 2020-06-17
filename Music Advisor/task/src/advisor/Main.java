ea55013a80533d8abc203ccf5f8e4d547adcf4a9
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-17 12:48:48 +0200

    Take user input and run appropriate blank method

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,7 +1,54 @@
 package advisor;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        determineAction();
     }
+
+    private static void determineAction() {
+        String action;
+        action = scanner.next();
+
+        switch (action) {
+            case "new" : {
+                printNewReleases();
+                break;
+            }
+            case "featured" : {
+                printFeaturedPlaylists();
+                break;
+            }
+            case "categories" : {
+                printAvailableCategories();
+                break;
+            }
+            case "playlists" : {
+                printCategoryPlaylists();
+                break;
+            }
+            case "exit" : {
+                exitApplication();
+            }
+        }
+    }
+
+    private static void printNewReleases() {
+    }
+
+    private static void printFeaturedPlaylists() {
+    }
+
+    private static void printAvailableCategories() {
+    }
+
+    private static void printCategoryPlaylists() {
+    }
+
+    private static void exitApplication() {
+    }
+
 }
