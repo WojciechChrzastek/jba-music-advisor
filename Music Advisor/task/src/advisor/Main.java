bc040beae5fd31f820cc04e59fc9ba1f1e7cbd8a
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-18 14:53:11 +0200

    Allow acctions if isAuthorized

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -30,74 +30,93 @@ public class Main {
 
     private static void executeAction() {
         String action = takeActionInput();
+
         switch (action) {
-            case "auth" : {
+            case "auth": {
                 printAuth();
                 break;
             }
-            case "new" : {
+            case "new": {
                 printNewReleases();
                 break;
             }
-            case "featured" : {
+            case "featured": {
                 printFeaturedPlaylists();
                 break;
             }
-            case "categories" : {
+            case "categories": {
                 printAvailableCategories();
                 break;
             }
-            default : {
+            default: {
                 printCategoryPlaylists(action);
                 break;
             }
-            case "exit" : {
+            case "exit": {
                 exitApplication();
             }
         }
     }
 
-    private static boolean printAuth() {
+    private static boolean verifyAuth() {
+        if (isAuthorized) {
+            return true;
+        } else {
+            System.out.println("Please, provide access for application.");
+            return false;
+        }
+    }
+
+    private static void printAuth() {
         String authLink = "authLink";
         System.out.println(authLink);
-        return isAuthorized = true;
+        isAuthorized = true;
+        executeAction();
     }
 
     private static void printNewReleases() {
-        System.out.println("---NEW RELEASES---\n" +
-                "Mountains [Sia, Diplo, Labrinth]\n" +
-                "Runaway [Lil Peep]\n" +
-                "The Greatest Show [Panic! At The Disco]\n" +
-                "All Out Life [Slipknot]");
+        if (verifyAuth()) {
+            System.out.println("---NEW RELEASES---\n" +
+                    "Mountains [Sia, Diplo, Labrinth]\n" +
+                    "Runaway [Lil Peep]\n" +
+                    "The Greatest Show [Panic! At The Disco]\n" +
+                    "All Out Life [Slipknot]");
+        }
         executeAction();
     }
 
     private static void printFeaturedPlaylists() {
-        System.out.println("---FEATURED---\n" +
-                "Mellow Morning\n" +
-                "Wake Up and Smell the Coffee\n" +
-                "Monday Motivation\n" +
-                "Songs to Sing in the Shower");
+        if (verifyAuth()) {
+            System.out.println("---FEATURED---\n" +
+                    "Mellow Morning\n" +
+                    "Wake Up and Smell the Coffee\n" +
+                    "Monday Motivation\n" +
+                    "Songs to Sing in the Shower");
+        }
         executeAction();
     }
 
     private static void printAvailableCategories() {
-        System.out.println("---CATEGORIES---\n" +
-                "Top Lists\n" +
-                "Pop\n" +
-                "Mood\n" +
-                "Latin");
+        if (verifyAuth()) {
+            System.out.println("---CATEGORIES---\n" +
+                    "Top Lists\n" +
+                    "Pop\n" +
+                    "Mood\n" +
+                    "Latin");
+        }
         executeAction();
     }
 
     private static void printCategoryPlaylists(String action) {
-        String playlistCategory = action.substring(action.indexOf(" ") + 1);
+        if (verifyAuth()) {
+            String playlistCategory = action.substring(action.indexOf(" ") + 1);
 
-        System.out.println("---" + playlistCategory.toUpperCase() + " PLAYLISTS---\n" +
-                "Walk Like A Badass\n" +
-                "Rage Beats\n" +
-                "Arab Mood Booster\n" +
-                "Sunday Stroll");
+            System.out.println("---" + playlistCategory.toUpperCase() + " PLAYLISTS---\n" +
+                    "Walk Like A Badass\n" +
+                    "Rage Beats\n" +
+                    "Arab Mood Booster\n" +
+                    "Sunday Stroll");
+        }
         executeAction();
     }
 
